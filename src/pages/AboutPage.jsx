5762914e4d684f7610a98328cd4259e6b71739ad
Cynthia <vaj.cynthia@gmail.com>
2026-03-09 23:37:35 -0500

    Update About Page - Commit 15

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,7 +2,14 @@ function AboutPage() {
   return (
     <section>
       <h2>About This App</h2>
-      <p>My Recipe Box is a simple React app built with Vite and React Router.</p>
+      <p>My Recipe Box is a React application designed to help users organize and manage their favorite recipes in one place. 
+        Users can add new recipes, edit or delete existing ones, mark favorites, and filter recipes by category. 
+        The app is built with React, Vite, and React Router, and stores recipe data in the browser using Local Storage so recipes remain saved between sessions.</p>
+
+        <br/>
+      
+        <p>App Created By Cynthia Johnsen</p>
+
     </section>
   );
 }
